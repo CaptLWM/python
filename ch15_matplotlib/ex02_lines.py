0dd75091ec04002e73c60e74f9af9225080b5d3f
CaptLWM <wonmin275@gmail.com>
2021-11-25 09:31:55 +0900

    Update ex02_lines.py

--- ch15_matplotlib/ex02_lines.py
+++ ch15_matplotlib/ex02_lines.py
@@ -16,7 +16,10 @@ plt.plot(x,y1,x,y2,x,y3)
 #plt.show()
 
 plt.plot(x,y1) # 처음 그리기 함수를 수행하면 그래프 창이 자동으로 생성된다
+'''figure1'''
 plt.figure() # 새로운 그래프 창을 생성한다
-
 plt.plot(x,y2)
+'''figure2'''
 plt.show()
+# 지금까지 만들어 놓은 모든 그래프 표시
+# plt.figure()로 새창 만드는게 좋음
